781960972b07fdc7160216048312f88a620091c2
Zhivotenko Oleksandr <zhivotenko.a.s@gmail.com>
2024-06-07 15:43:50 +0300

    fix: minor fixes

--- test/system.test.js
+++ test/system.test.js
@@ -1,17 +1,18 @@
 const assert = require('assert');
-const {request} = require('gaxios');
+const { request } = require('gaxios');
 
 const port = process.env.PORT || '8080';
 const url = process.env.SERVICE_URL || `http://localhost:${port}`;
 const token = process.env.TOKEN || '';
+const timeout = process.env.TIMEOUT || 5000;
 
 describe('Hello World', () => {
   it('can respond to an HTTP request', async () => {
     console.log(`    - Requesting GET ${url}/...`);
     const res = await request({
       url: url + '/',
-      headers: {"Authorization": "Bearer " + token},
-      timeout: 5000,
+      headers: { "Authorization": "Bearer " + token },
+      timeout: timeout,
     });
 
     assert.equal(res.status, '200');
